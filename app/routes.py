ac4ec2c30c8d5f8ab501d4534193099581cf8904
Patrick Harding <patrickhardingr@gmail.com>
2021-06-09 14:10:00 -0700

    improved base template and styles

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,35 @@ from app.forms import LoginForm, PostForm
 @app.route('/')
 @app.route('/index')
 def index():
+	user = {"username" : "Pat", "age" : 10, "grade" : "A"}
+	header = "Home Page"
+	return render_template('index.html', header=header, user=user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	header="Log In"
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user {}, remember_me={}'.format(
+			form.username.data, form.remember_me.data))
+		return redirect(url_for('index'))
+
+	return render_template('login.html', header=header, form=form)
+
+@app.route('/post')
+def post():
+	header="Post"
+	form = PostForm()
+	return render_template('post.html', header=header, form=form)
+
+@app.route('/about')
+def about():
+	header="About Me"
+	return render_template('about.html', header=header)
+
+@app.route('/sideprojects')
+def sideprojects():
+	header="Side Projects"
 	#Mock object creation
 	user = {"username" : "Pat", "age" : 10, "grade" : "A"}
 	#More mock object creation: posts that contain an author, body of text, and date
@@ -15,24 +44,4 @@ def index():
 			   'body': 'This is one of the first posts on this blog',
 			   'date' : '6/3/2021'}]
 
-
-	return render_template('index.html', title='Home Page', user=user, posts=posts)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		flash('Login requested for user {}, remember_me={}'.format(
-			form.username.data, form.remember_me.data))
-		return redirect(url_for('index'))
-
-	return render_template('login.html', title='Log In', form=form)
-
-@app.route('/post')
-def post():
-	form = PostForm()
-	return render_template('post.html', title='Post', form=form)
-
-@app.route('/about')
-def about():
-	return render_template('about.html', title ='About Me', header='About Me')
+	return render_template('sideprojects.html', user=user, posts=posts, header=header)
